095ced771ecb4a1890a5835de94cdecd07ec4abe
tomya <tarrinda230@gmail.com>
2023-03-16 19:28:29 -0300

    eureka-fixed

--- eureka-server/src/main/java/com/dh/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/dh/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.dh.eurekaserver;
+package com.dh.eurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
 }
--- eureka-server/src/test/java/com/dh/eurekaserver/eurekaServer/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/dh/eurekaserver/eurekaServer/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dh.eurekaserver;
+package com.dh.eurekaServer.eurekaServer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EurekaServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
